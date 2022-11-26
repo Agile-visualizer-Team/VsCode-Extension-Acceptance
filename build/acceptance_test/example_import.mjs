81d74bf58758d4067de8766011e97f4f8ae85f80
instafiore <fiorentinosalvatore65@gmail.com>
2022-11-26 22:42:56 +0100

    modified .npmrc

---
+++ build/acceptance_test/example_import.mjs
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// How to install package -> npm install @agile-visualizer-team/vscode-extension-playground
+const matrix_visualization_js_1 = require("@agile-visualizer-team/agile_visualizer_matrix_visualizaton/src/matrix_visualization.js");
+let script = new matrix_visualization_js_1.MatrixCreator();
+script.run_script();
+// import { MatrixCreator } from "@agile-visualizer-team/vscode-extension-playground/src/matrix_visualization";
+//sha512-4uFxOqyWC7X9Tn1VMBCWrDRIgrAmt4aWmwuk+uYIaGBqwUAeuxUUDKkwcluWrFaTcnY9Zbr5Zf8ppADrlCQwGQ=="
--- src/acceptance_test/example_import.mjs
+++ src/acceptance_test/example_import.mjs
@@ -1,6 +1,6 @@
+// How to install package -> npm install @agile-visualizer-team/vscode-extension-playground
 import { MatrixCreator } from "@agile-visualizer-team/agile_visualizer_matrix_visualizaton/src/matrix_visualization.js";
 let script = new MatrixCreator();
 
 script.run_script();
-
 // import { MatrixCreator } from "@agile-visualizer-team/vscode-extension-playground/src/matrix_visualization";
