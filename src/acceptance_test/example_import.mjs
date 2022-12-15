e1dee49d200f021a960f6fa91d8a3bca1af95435
instafiore <fiorentinosalvatore65@gmail.com>
2022-12-15 16:19:42 +0100

    test repo

--- src/acceptance_test/example_import.mjs
+++ src/acceptance_test/example_import.mjs
@@ -1,5 +1,1 @@
-import { MatrixCreator } from "@agile-visualizer-team/agile_visualizer_matrix_visualizaton/src/matrix_visualization.js";
-let script = new MatrixCreator();
-
-script.run_script();
-// import { MatrixCreator } from "@agile-visualizer-team/vscode-extension-playground/src/matrix_visualization";
+import { DLVWrapper } from '@agile-visualizer-team/visualizer-asp/src/wrapper-dlv/dlv_wrapper'
